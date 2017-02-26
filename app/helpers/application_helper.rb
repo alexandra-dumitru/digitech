498239c05b811c1be7bf0f6fa4fbd1631b788379
alexandra-dumitru <alex_cd91@yahoo.com>
2017-02-26 14:52:45 +0200

    sign up page created

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	#Full title of a page
+	def full_title(page_title = '')
+    base_title = "Digitech Apple"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  root 'application#hello'
+  get '/home', to: 'static_pages#home'
+  get '/contact', to: 'static_pages#contact'
+
   resources :promocodes
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
