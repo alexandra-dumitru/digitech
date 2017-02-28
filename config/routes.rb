c1767f97eb93c5532f7b84fd0c549303e128237f
alexandra-dumitru <alex_cd91@yahoo.com>
2017-02-28 21:31:49 +0200

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  get 'sessions/new'
+
+  root 'static_pages#home'
   get '/home', to: 'static_pages#home'
   get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
+  get '/login',   to: 'sessions#new'
+  post '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :promocodes
   resources :users
