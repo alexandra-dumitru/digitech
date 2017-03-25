be8a29a7f63029c784edd5dfacba6cc15fcf72db
Alexandra <alex_cd91@yahoo.com>
2017-03-25 14:32:34 +0200

    reset password done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,8 +54,8 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :user_name =>  ENV["SENDGRID_EMAIL"],
-    :password =>  ENV["SENDGRID_PASSWORD"],
+    :user_name => ENV["SENDGRID_EMAIL"],
+    :password => ENV["SENDGRID_PASSWORD"],
     :domain => 'heroku.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
